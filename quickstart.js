6584fba0488af968a37bf589b530ffa9671b5a9c
shubhregex <63728452+shubhregex@users.noreply.github.com>
2020-10-15 15:21:55 +0000

    updates

--- quickstart.js
+++ quickstart.js
@@ -4,84 +4,86 @@
   var outputVolumeBar = document.getElementById('output-volume');
   var inputVolumeBar = document.getElementById('input-volume');
   var volumeIndicators = document.getElementById('volume-indicators');
-  
-  var keys = $.getJSON('keys.json');
   var device;
-
-  log('Requesting Capability Token...');
-  $.getJSON(keys.functionURL)
-    .then(function (data) {
-      log('Got a token.');
-      console.log('Token: ' + data.token);
-
-      // Setup Twilio.Device
-      device = new Twilio.Device(data.token, {
-        // Set Opus as our preferred codec. Opus generally performs better, requiring less bandwidth and
-        // providing better audio quality in restrained network conditions. Opus will be default in 2.0.
-        codecPreferences: ['opus', 'pcmu'],
-        // Use fake DTMF tones client-side. Real tones are still sent to the other end of the call,
-        // but the client-side DTMF tones are fake. This prevents the local mic capturing the DTMF tone
-        // a second time and sending the tone twice. This will be default in 2.0.
-        fakeLocalDTMF: true,
-        // Use `enableRingingState` to enable the device to emit the `ringing`
-        // state. The TwiML backend also needs to have the attribute
-        // `answerOnBridge` also set to true in the `Dial` verb. This option
-        // changes the behavior of the SDK to consider a call `ringing` starting
-        // from the connection to the TwiML backend to when the recipient of
-        // the `Dial` verb answers.
-        enableRingingState: true,
-      });
-
-      device.on('ready',function (device) {
-        log('Twilio.Device Ready!');
-        document.getElementById('call-controls').style.display = 'block';
-      });
-
-      device.on('error', function (error) {
-        log('Twilio.Device Error: ' + error.message);
-      });
-
-      device.on('connect', function (conn) {
-        log('Successfully established call!');
-        document.getElementById('button-call').style.display = 'none';
-        document.getElementById('button-hangup').style.display = 'inline';
-        volumeIndicators.style.display = 'block';
-        bindVolumeIndicators(conn);
-      });
-
-      device.on('disconnect', function (conn) {
-        log('Call ended.');
-        document.getElementById('button-call').style.display = 'inline';
-        document.getElementById('button-hangup').style.display = 'none';
-        volumeIndicators.style.display = 'none';
-      });
-
-      device.on('incoming', function (conn) {
-        log('Incoming connection from ' + conn.parameters.From);
-        var archEnemyPhoneNumber = '+12093373517';
-
-        if (conn.parameters.From === archEnemyPhoneNumber) {
-          conn.reject();
-          log('It\'s your nemesis. Rejected call.');
-        } else {
-          // accept the incoming connection and start two-way audio
-          conn.accept();
-        }
-      });
-
-      setClientNameUI(data.identity);
-
-      device.audio.on('deviceChange', updateAllDevices.bind(device));
-
-      // Show audio selection UI if it is supported by the browser.
-      if (device.audio.isOutputSelectionSupported) {
-        document.getElementById('output-selection').style.display = 'block';
-      }
-    })
-    .catch(function (err) {
-      console.log(err);
-      log('Could not get a token from server!');
+  
+  $.getJSON('keys.json')
+    .then(function(keys) {
+      log('Requesting Capability Token...');
+      $.getJSON(keys.functionURL)
+        .then(function (data) {
+          log('Got a token.');
+          console.log('Token: ' + data.token);
+    
+          // Setup Twilio.Device
+          device = new Twilio.Device(data.token, {
+            // Set Opus as our preferred codec. Opus generally performs better, requiring less bandwidth and
+            // providing better audio quality in restrained network conditions. Opus will be default in 2.0.
+            codecPreferences: ['opus', 'pcmu'],
+            // Use fake DTMF tones client-side. Real tones are still sent to the other end of the call,
+            // but the client-side DTMF tones are fake. This prevents the local mic capturing the DTMF tone
+            // a second time and sending the tone twice. This will be default in 2.0.
+            fakeLocalDTMF: true,
+            // Use `enableRingingState` to enable the device to emit the `ringing`
+            // state. The TwiML backend also needs to have the attribute
+            // `answerOnBridge` also set to true in the `Dial` verb. This option
+            // changes the behavior of the SDK to consider a call `ringing` starting
+            // from the connection to the TwiML backend to when the recipient of
+            // the `Dial` verb answers.
+            enableRingingState: true,
+          });
+    
+          device.on('ready',function (device) {
+            log('Twilio.Device Ready!');
+            document.getElementById('call-controls').style.display = 'block';
+          });
+    
+          device.on('error', function (error) {
+            log('Twilio.Device Error: ' + error.message);
+          });
+    
+          device.on('connect', function (conn) {
+            log('Successfully established call!');
+            document.getElementById('button-call').style.display = 'none';
+            document.getElementById('button-hangup').style.display = 'inline';
+            volumeIndicators.style.display = 'block';
+            bindVolumeIndicators(conn);
+          });
+    
+          device.on('disconnect', function (conn) {
+            log('Call ended.');
+            document.getElementById('button-call').style.display = 'inline';
+            document.getElementById('button-hangup').style.display = 'none';
+            volumeIndicators.style.display = 'none';
+          });
+    
+          device.on('incoming', function (conn) {
+            log('Incoming connection from ' + conn.parameters.From);
+            var archEnemyPhoneNumber = '+12093373517';
+    
+            if (conn.parameters.From === archEnemyPhoneNumber) {
+              conn.reject();
+              log('It\'s your nemesis. Rejected call.');
+            } else {
+              // accept the incoming connection and start two-way audio
+              conn.accept();
+            }
+          });
+    
+          setClientNameUI(data.identity);
+    
+          device.audio.on('deviceChange', updateAllDevices.bind(device));
+    
+          // Show audio selection UI if it is supported by the browser.
+          if (device.audio.isOutputSelectionSupported) {
+            document.getElementById('output-selection').style.display = 'block';
+          }
+        })
+        .catch(function (err) {
+          console.log(err);
+          log('Could not get a token from server!');
+        });
     });
+
 
   // Bind button to make call
   document.getElementById('button-call').onclick = function () {
